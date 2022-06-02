253d7b1c471375573a1a88bc7c1d128dbe235ec5
andresdelrio <andresdelrio@localhost>
2022-06-02 14:15:57 -0400

    Asociaciones de requisito en auditoría, en la sección de alcance

---
+++ SQL-Genericas/requisitodeauditoria.sql
@@ -0,0 +1,30 @@
+SELECT
+/**
+Creación:
+2022-06-02. Andrés Del Río. Lista los requisitos asociados a las auditorías en el alcance de auditoría
+
+**/
+        AUAUDIT.IDAUDIT,
+        AUAUDIT.NMAUDIT,
+        RQREQUIREMENT.IDREQUIREMENT,
+        RQREQUIREMENT.NMREQUIREMENT
+    FROM
+        AUAUDIT                                                                                          
+    INNER JOIN
+        AUSCOPESTRUCT                                                                                                  
+            ON (
+                AUSCOPESTRUCT.CDSCOPEDEFINITION=AUAUDIT.CDSCOPEDEFINITION                                                                                                 
+            )                                                                                          
+    INNER JOIN
+        AUSTRUCTASSOC                                                                                                  
+            ON (
+                AUSTRUCTASSOC.CDSTRUCT=AUSCOPESTRUCT.CDSTRUCT                                                                                                 
+            )                                                                                         
+    INNER JOIN
+        RQREQUIREMENT                                                                                                 
+            ON (
+                RQREQUIREMENT.CDREQUIREMENT = AUSTRUCTASSOC.CDREQUIREMENT
+                AND RQREQUIREMENT.CDREVISION =  AUSTRUCTASSOC.CDREQUIREMENTREVISION
+            )                                                                                          
+    WHERE
+        AUSCOPESTRUCT.CDSTRUCT=AUSCOPESTRUCT.CDSTRUCTROOT
