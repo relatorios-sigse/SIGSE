3d21c58f9cab023b79c4fe903e8ef0e6651669e4
andresdelrio <andresdelrio@192.168.0.4>
2022-06-06 15:45:52 -0400

    Desarrollada durante paso a productivo de boletines y normativa ucsc

---
+++ SQL-Genericas/atributosdecategorias.sql
@@ -0,0 +1,25 @@
+SELECT
+/**
+Creación:
+2022-06-06. Andrés Del Río. Permite listar las categorías asociados a un atributo específico.
+Si se quita el filtro (todo lo que hay a partir del WHERE), listará todos los pares categoría-atributo
+relacionados.
+
+**/
+        C.IDCATEGORY,
+        C.NMCATEGORY,
+        AC.FGREQUIRED,
+		AC.NRORDER,
+		AC.FGREADONLY,
+		A.NMATTRIBUTE,
+		A.NMLABEL
+    FROM
+        DCCATDOCATTRIB AC 
+    JOIN
+        DCCATEGORY C 
+            ON C.CDCATEGORY = AC.CDCATEGORY 
+    JOIN
+        ADATTRIBUTE A 
+            ON A.CDATTRIBUTE = AC.CDATTRIBUTE 
+    WHERE
+        A.NMATTRIBUTE = 'SD7-BOLETIN'
