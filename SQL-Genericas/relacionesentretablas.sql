0a8fb20df2f6ed04558339e74da5ef4c867940d3
andresdelrio <andresdelrio@192.168.0.3>
2022-07-13 15:46:13 -0400

    Para explorar el modelo de datos de una base Postgres

---
+++ SQL-Genericas/relacionesentretablas.sql
@@ -0,0 +1,26 @@
+SELECT
+/**
+Creación:
+2022-07-13. Andrés Del Río. Query que permite explorar el modelo de datos, pasando como parámetro
+el nombre de la tabla de la relación foránea, listando todas las tablas que hacen referencia a la primera.
+DB: Postgres
+**/
+        tc.table_schema,
+        tc.constraint_name,
+        tc.table_name,
+        kcu.column_name,
+        ccu.table_schema AS foreign_table_schema,
+        ccu.table_name AS foreign_table_name,
+        ccu.column_name AS foreign_column_name  
+    FROM
+        information_schema.table_constraints AS tc      
+    JOIN
+        information_schema.key_column_usage AS kcu       
+            ON tc.constraint_name = kcu.constraint_name       
+            AND tc.table_schema = kcu.table_schema     
+    JOIN
+        information_schema.constraint_column_usage AS ccu       
+            ON ccu.constraint_name = tc.constraint_name       
+            AND ccu.table_schema = tc.table_schema 
+    WHERE
+        ccu.table_name = 'gnassoc'
